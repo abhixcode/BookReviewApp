6a9e5f9157be7bf1b25e421a676170fe99cec4f3
Ubuntu <ubuntu@ip-172-31-43-250.ec2.internal>
2021-04-04 23:18:37 +0000

    changed config setup

--- bookreview/config.py
+++ bookreview/config.py
@@ -1,5 +1,11 @@
 import os
+import json
+
+with open('/etc/config/bookreview_config.json') as config_file:
+    config = json.load(config_file)
 
 class Config:
-	SECRET_KEY = os.environ.get('SECRET_KEY')
-	SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+	#SECRET_KEY = os.environ.get('SECRET_KEY')
+        SECRET_KEY = config.get('SECRET_KEY')
+	#SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+        SQLALCHEMY_DATABASE_URI = config.get('SQLALCHEMY_DATABASE_URI')
